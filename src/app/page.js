8ad86c8128da279a4fc46824ef03cc79c3836616
ronakvarshney <ronakvarshney7100@gmail.com>
2025-05-02 15:17:12 +0530

    more changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function page(){
   return(
     <div>
       Hello , Myself Ronak Varshney
-      <Link href='/students'>Go to Students</Link>
+      <Link href='/students' className="m-10 bg-blue-600 p-2">Go to Students</Link>
     </div>
   )
 }
