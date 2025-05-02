45700755ad37b103f1ac57a2e9ca4a6e396fe7f1
ronakvarshney <ronakvarshney7100@gmail.com>
2025-05-02 15:15:03 +0530

    update main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function page(){
+  
   return(
     <div>
-      hello
+      Hello , Myself Ronak Varshney
+      <Link href='/students'>Go to Students</Link>
     </div>
   )
 }
